44f8e57a5e38a74a93a7cd4837a80b2789ed8d82
Yannik Rödel <hey@yannik.info>
2021-06-23 19:03:37 +0200

    Fix injection in VForm

--- src/interface/core/forms/VForm.vue
+++ src/interface/core/forms/VForm.vue
@@ -22,14 +22,13 @@ import {
   ref,
   UnwrapRef,
 } from 'vue';
-import { ReadonlyRefInjectionKey } from '@/utils';
 
 interface FormContext {
   validationVisible: true;
   useInput: (validity: Ref<Readonly<boolean>>) => void;
 }
 
-export const Form = ReadonlyRefInjectionKey<UnwrapRef<FormContext>>();
+export const Form: InjectionKey<Readonly<UnwrapRef<FormContext>>> = Symbol();
 
 export default defineComponent({
   name: 'VForm',
